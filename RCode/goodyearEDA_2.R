ddf37251cf6adfb39b525abe08a04ee5920e0f35
ivan <kositvan+git@gmail.com>
2020-06-10 04:44:01 +0800

    refactored selenium vs date, and added sel vs netflow
    1. refactored the code for drawing selenium vs date for train 1 and 4
    2. added images of selenium vs date
    3. added netflow for analysis
    4. did linear regression on netflow vs selenium for train 1 and 4

--- RCode/goodyearEDA_2.R
+++ RCode/goodyearEDA_2.R
@@ -16,39 +16,24 @@ glimpse(dfData)
 dfDataC <- dfData[!is.na(dfData$Selenium), ]
 glimpse(dfDataC)
 
+#------------------------------------------------------
+
 #set periods
 period1End <- ymd( "2012-03-01")
 period2End <- ymd( "2015-04-01")
 period3End <- ymd( "2017-04-01" )
 periods <- c(period1End, period2End, period3End)
 
-
-#Draw everything against each other
-colnames(dfDataC)
-
-for(i in colnames(dfDataC)){
-    cur_plot <- ggplot(data = dfDataC, aes_string(x = i, y = "Selenium")) + #aes_string because variable is not actual column factor term, but a name as string.
-        geom_line(color = ID ) + 
-        geom_point(alpha = 0.3) + 
-        geom_smooth(method = lm, color = "blue")
-    print(cur_plot) # autoprint is turned off so we need to print it
-    #Sys.sleep(2) #sleep so that we can see the output if needed
-    
-}
-
-
-
 # Only looking at bin1, bin5, bin7 and brine - same train
-drawTrain1FromPeriod <- function(startPeriod, endPeriod){
+drawTrain1FromPeriod <- function(startPeriod, endPeriod, interval_string){
     resultPlot <- dfDataC %>% 
         filter(dfDataC$ID == "Bin1" | dfDataC$ID == "Bin5" | dfDataC$ID == "Bin7" | 
                    dfDataC$ID == "brine") %>% 
         filter(date >= startPeriod & date <= endPeriod ) %>% 
         ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
         geom_point(size = 2) +  
-        geom_line(size = 1.5) +
         geom_line(aes(y = 0.002), color = "red", alpha = 0.5) +
-        scale_x_date(date_breaks = "6 months", date_labels = "%b %Y") +
+        scale_x_date(date_breaks = interval_string, date_labels = "%b %Y") +
         scale_color_viridis_d() +
         xlab("Date") +
         ylab("Selenium Content (mg/L)")
@@ -58,53 +43,50 @@ drawTrain1FromPeriod <- function(startPeriod, endPeriod){
 
 #Selenium contents for train 1 for differnt periods
 #Period 1
-plotT1P1 <- drawTrain1FromPeriod(dfDataC$date[1], period1End) + 
-    labs(title= "Selenium content for Train 1 for period 1")
+plotT1P1 <- drawTrain1FromPeriod(dfDataC$date[1], period1End, "4 months") + 
+    labs(title= "Selenium content of Train 1 for period 1")
+plotT1P1
 
 #Period 2
-plotT1P2 <- drawTrain1FromPeriod(period1End, period2End) + 
-    labs(title= "Selenium content for Train 1 for period 2")
+plotT1P2 <- drawTrain1FromPeriod(period1End, period2End, "4 months") + 
+    labs(title= "Selenium content of Train 1 for period 2")
+plotT1P2
 
 #Period 3
-plotT1P3 <- drawTrain1FromPeriod(period2End, period3End) + 
-    labs(title= "Selenium content for Train 1 for period 3")
+plotT1P3 <- drawTrain1FromPeriod(period2End, period3End, "4 months") + 
+    labs(title= "Selenium content of Train 1 for period 3")
+plotT1P3
 
 #Period 4
-plotT1P4 <- dfDataC %>% 
-    filter(dfDataC$ID == "Bin1" | dfDataC$ID == "Bin5" | dfDataC$ID == "Bin7" | 
-               dfDataC$ID == "brine") %>% 
-    filter(date >= period3End) %>% 
-    ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
-    geom_point(size = 2) +  
-    geom_line(size = 1.5) +
-    geom_line(aes(y = 0.002), color = "red", alpha = 0.5) +
-    scale_x_date(date_breaks = "1 month", date_labels = "%b %y") +
-    scale_color_viridis_d() +
-    xlab("Date") +
-    ylab("Selenium Content (mg/L)") + 
-    labs(title= "Selenium content for Train 1 for period 4")
+plotT1P4 <- drawTrain1FromPeriod(period3End, tail(dfDataC$date, n = 1), "1 month") + 
+    labs(title= "Selenium content of Train 1 for period 4")
+plotT1P4
 
 
-g1 <- ggplotGrob(plotT1P1)
-g2 <- ggplotGrob(plotT1P2)
-g3 <- ggplotGrob(plotT1P3)
-g4 <- ggplotGrob(plotT1P4)
-gr1 <- rbind(g1, g2)
-gr2 <- rbind(g3, g4)
-g <- cbind(gr1, gr2)
-grid.newpage()
-grid.draw(g)
+#draw grid function
+drawGrid2x2 <- function(plot1, plot2, plot3, plot4){
+    g1 <- ggplotGrob(plot1)
+    g2 <- ggplotGrob(plot2)
+    g3 <- ggplotGrob(plot3)
+    g4 <- ggplotGrob(plot4)
+    gr1 <- rbind(g1, g2)
+    gr2 <- rbind(g3, g4)
+    g <- cbind(gr1, gr2)
+    grid.newpage()
+    grid.draw(g)    
+}
+
+drawGrid2x2(plotT1P1, plotT1P2, plotT1P3, plotT1P4)
 
 #Selenium contents for train 4 for differnt periods
-drawTrain4FromPeriod <- function(startPeriod, endPeriod){
+drawTrain4FromPeriod <- function(startPeriod, endPeriod, interval_string){
     resultPlot <- dfDataC %>% 
         filter(dfDataC$ID == "Bin3" | dfDataC$ID == "brine") %>% 
         filter(date >= startPeriod & date <= endPeriod ) %>% 
         ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
         geom_point(size = 2) +  
-        geom_line(size = 1.5) +
         geom_line(aes(y = 0.002), color = "red", alpha = 0.5) +
-        scale_x_date(date_breaks = "6 months", date_labels = "%b %y") +
+        scale_x_date(date_breaks = interval_string, date_labels = "%b %y") +
         scale_color_viridis_d() +
         xlab("Date") +
         ylab("Selenium Content (mg/L)")
@@ -114,37 +96,55 @@ drawTrain4FromPeriod <- function(startPeriod, endPeriod){
 
 #Selenium contents for train 4 for differnt periods
 #Period 1
-plotT4P1 <- drawTrain4FromPeriod(dfDataC$date[1], period1End) + 
-    labs(title= "Selenium content for Train 4 for period 1")
+plotT4P1 <- drawTrain4FromPeriod(dfDataC$date[1], period1End, "4 months") + 
+    labs(title= "Selenium content of Train 4 for period 1")
+plotT4P1
 
 #Period 2
-plotT4P2 <- drawTrain4FromPeriod(period1End, period2End) + 
-    labs(title= "Selenium content for Train 4 for period 2")
+plotT4P2 <- drawTrain4FromPeriod(period1End, period2End, "4 months") + 
+    labs(title= "Selenium content of Train 4 for period 2")
+plotT4P2
 
 #Period 3
-plotT4P3 <- drawTrain4FromPeriod(period2End, period3End) + 
-    labs(title= "Selenium content for Train 4 for period 3")
+plotT4P3 <- drawTrain4FromPeriod(period2End, period3End, "4 months") + 
+    labs(title= "Selenium content of Train 4 for period 3")
+plotT4P3
 
 #Period 4
-plotT4P4 <- dfDataC %>% 
-    filter(dfDataC$ID == "Bin3" | dfDataC$ID == "brine") %>% 
-    filter(date >= period3End) %>% 
-    ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
-    geom_point(size = 2) +  
-    geom_line(size = 1.5) +
-    geom_line(aes(y = 0.002), color = "red", alpha = 0.5) +
-    scale_x_date(date_breaks = "1 month", date_labels = "%b %Y") +
-    scale_color_viridis_d() +
-    xlab("Date") +
-    ylab("Selenium Content (mg/L)") + 
-    labs(title= "Selenium content for Train 4 for period 4")
+plotT4P4 <- drawTrain4FromPeriod(period3End, tail(dfDataC$date, n = 1), "1 month") + 
+    labs(title= "Selenium content of Train 4 for period 4")
+plotT4P4
 
-g1 <- ggplotGrob(plotT4P1)
-g2 <- ggplotGrob(plotT4P2)
-g3 <- ggplotGrob(plotT4P3)
-g4 <- ggplotGrob(plotT4P4)
-gr1 <- rbind(g1, g2)
-gr2 <- rbind(g3, g4)
-g <- cbind(gr1, gr2)
-grid.newpage()
-grid.draw(g)
+drawGrid2x2(plotT4P1, plotT4P2, plotT4P3, plotT4P4)
+
+#--------------------------------------
+
+#cleaning inflow and outflow
+dfDataFlow <- dfDataC[!is.na(dfDataC$Inflow), ]
+dfDataFlow <- dfDataFlow[!is.na(dfDataFlow$Outflow), ]
+
+dfDataFlow <- add_column(dfDataFlow, netflow = (dfDataFlow$Outflow - dfDataFlow$Inflow) ) 
+# glimpse(dfDataFlow)
+
+# regression analysis of Netflow vs Selenium for Train 1
+dfDataFlow %>% 
+    filter(ID == "Bin1" | ID == "Bin5" | ID == "Bin7") %>% 
+    ggplot(aes(Selenium, netflow)) +
+    geom_point(alpha = 1, aes(color = ID)) +
+    geom_smooth(formula = y~x, method = "lm") + # formula always look at the varaible in terms of x
+    scale_color_viridis_d("Selenium and netflow") +
+    xlab("selenium") +  
+    ylab("netflow") +
+    labs(title= "Selenium vs netflow RA for Train 1")
+
+# regression analysis of Netflow vs Selenium for Train 4
+dfDataFlow %>% 
+    filter(ID == "Bin3") %>% 
+    ggplot(aes(Selenium, netflow)) +
+    geom_point(alpha = 1, aes(color = ID)) +
+    geom_smooth(formula = y~x, method = "lm") + # formula always look at the varaible in terms of x
+    scale_color_viridis_d("Selenium and netflow") +
+    xlab("selenium") + 
+    ylab("netflow") +
+    labs(title= "Selenium vs netflow RA for Train 4")
+
