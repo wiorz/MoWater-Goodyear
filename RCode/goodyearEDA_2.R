5fa737f068664bded1851053e0e5f3145c077b7e
ivan <kositvan+git@gmail.com>
2020-06-11 04:16:24 +0800

    Updated code

--- RCode/goodyearEDA_2.R
+++ RCode/goodyearEDA_2.R
@@ -10,38 +10,50 @@ suppressMessages(library( fields))
 
 #head(goodyear)
 
+# Prep for matching size first! Some bins have anextra line of NA, row num 
+# should be 438.
+# Bins with extra rows: bin1, bin2, bin3, bin6
+Bin1 <- Bin1[1:nrow(Bin1) - 1, ]
+Bin2 <- Bin2[1:nrow(Bin1) - 1, ]
+Bin3 <- Bin3[1:nrow(Bin1) - 1, ]
+Bin6 <- Bin6[1:nrow(Bin1) - 1, ]
+
+
 dfData <- as_tibble(goodyear)
 glimpse(dfData)
 
 dfDataSel <- dfData[!is.na(dfData$Selenium), ]
 glimpse(dfDataSel)
 
+dfDataAr <- dfData[!is.na(dfData$Arsenic), ]
+
+
 #------------------------------------------------------
 
 #set periods
 period1End <- ymd( "2012-03-01")
-period2End <- ymd( "2015-04-01")
-period3End <- ymd( "2017-04-01" )
+period2End <- ymd( "2015-01-01")
+period3End <- ymd( "2017-04-01")
 periods <- c(period1End, period2End, period3End)
 
 # Only looking at brine and train 1 (bin1, bin5, bin7)
 drawTrain1FromPeriod <- function(startPeriod, endPeriod, interval_string){
     resultPlot <- dfDataSel %>% 
-        filter(dfDataSel$ID == "Bin1" | dfDataSel$ID == "Bin5" | dfDataSel$ID == "Bin7" | 
-                   dfDataSel$ID == "brine") %>% 
-        filter(date >= startPeriod & date <= endPeriod ) %>% 
+        filter(dfDataSel$ID == "Bin1" | dfDataSel$ID == "Bin5" | 
+                   dfDataSel$ID == "Bin7" | dfDataSel$ID == "brine") %>% 
+        filter(date >= startPeriod & date < endPeriod ) %>% 
         ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
+        geom_line() + 
         geom_point(size = 2) +  
         geom_line(aes(y = 0.002), color = "red", alpha = 0.5) +
         scale_x_date(date_breaks = interval_string, date_labels = "%b %Y") +
-        scale_color_viridis_d() +
         xlab("Date") +
         ylab("Selenium Content (mg/L)")
     
     return(resultPlot) 
 }
 
-#Selenium contents for train 1 for differnt periods
+#Selenium contents for train 1 for different periods
 #Period 1
 plotT1P1 <- drawTrain1FromPeriod(dfDataSel$date[1], period1End, "4 months") + 
     labs(title= "Selenium content of Train 1 for period 1")
@@ -58,7 +70,8 @@ plotT1P3 <- drawTrain1FromPeriod(period2End, period3End, "4 months") +
 plotT1P3
 
 #Period 4
-plotT1P4 <- drawTrain1FromPeriod(period3End, tail(dfDataSel$date, n = 1), "1 month") + 
+plotT1P4 <- drawTrain1FromPeriod(period3End, 
+                                 tail(dfDataSel$date, n = 1) + 1, "1 month") + #the tail() +1 is adding one day to the last day in the dataset
     labs(title= "Selenium content of Train 1 for period 4")
 plotT1P4
 
@@ -85,12 +98,12 @@ drawGrid2x2(plotT1P1, plotT1P2, plotT1P3, plotT1P4)
 drawTrain4FromPeriod <- function(startPeriod, endPeriod, interval_string){
     resultPlot <- dfDataSel %>% 
         filter(dfDataSel$ID == "Bin3" | dfDataSel$ID == "brine") %>% 
-        filter(date >= startPeriod & date <= endPeriod ) %>% 
-        ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
+        filter(date >= startPeriod & date < endPeriod ) %>% 
+        ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.
+        geom_line() + 
         geom_point(size = 2) +  
         geom_line(aes(y = 0.002), color = "red", alpha = 0.5) +
         scale_x_date(date_breaks = interval_string, date_labels = "%b %y") +
-        scale_color_viridis_d() +
         xlab("Date") +
         ylab("Selenium Content (mg/L)")
     
@@ -114,7 +127,8 @@ plotT4P3 <- drawTrain4FromPeriod(period2End, period3End, "4 months") +
 plotT4P3
 
 #Period 4
-plotT4P4 <- drawTrain4FromPeriod(period3End, tail(dfDataSel$date, n = 1), "1 month") + 
+plotT4P4 <- drawTrain4FromPeriod(period3End, 
+                                 tail(dfDataSel$date, n = 1) + 1, "1 month") + 
     labs(title= "Selenium content of Train 4 for period 4")
 plotT4P4
 
@@ -125,9 +139,10 @@ drawGrid2x2(plotT4P1, plotT4P2, plotT4P3, plotT4P4)
 #cleaning inflow and outflow
 dfDataSelFlow <- dfDataSel[!is.na(dfDataSel$Inflow), ]
 dfDataSelFlow <- dfDataSelFlow[!is.na(dfDataSelFlow$Outflow), ]
-
+#make the result of outflow-inflow as a new column in the data
 dfDataSelFlow <- add_column(dfDataSelFlow, 
-                            netflow = (dfDataSelFlow$Outflow - dfDataSelFlow$Inflow) ) 
+                            netflow = (dfDataSelFlow$Outflow - 
+                                           dfDataSelFlow$Inflow) ) 
 # glimpse(dfDataFlow)
 
 # regression analysis of Netflow vs Selenium for Train 1
@@ -152,4 +167,27 @@ dfDataSelFlow %>%
     ylab("netflow") +
     labs(title= "Selenium vs netflow RA for Train 4")
 
+#----------------------------------------------
 
+
+
+
+#All variables to be excluded from subtraction:
+#   date, Inflow, Outflow, Temp..Celsius, Color, COD, Conductivity.S.m, DO.mg.L, 
+#   ORP, pH
+tempBin1 <- subset(Bin5, select = -c(date, Inflow, Outflow, Temp..Celsius, 
+                                     Color, COD, Conductivity.S.m, DO.mg.L, 
+                                     ORP, pH) )
+tempBin2 <- subset(Bin1, select = -c(date, Inflow, Outflow, Temp..Celsius, 
+                                     Color, COD, Conductivity.S.m, DO.mg.L, 
+                                     ORP, pH) )
+
+binMinus1from5 <- (tempBin2 - tempBin1)
+binMinus1from5 <- add_column(binMinus1from5, date = Bin1$date) 
+binMinus1from5 <- as_tibble(binMinus1from5)
+
+binMinus1from5C <- binMinus1from5[!(is.na(binMinus1from5$Selenium)), ]
+
+binMinus1from5C %>% 
+    ggplot(aes(x = date, y = Selenium, group = month(date))) +
+    geom_line()
