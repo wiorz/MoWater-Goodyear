450984fb119e9aaab1499d3e0c0eb013dda97d32
ivan <kositvan+git@gmail.com>
2020-06-11 00:12:20 +0800

    updated datasets, small edit to goodyearEDA2
    1. new dataset goodyearMoWater.rda with < values replaced with their immediate values.
    2. Old files now have 0 behind the names, i.e. createBinObject.r   (new)
    createBinObject0.r  (old)
    goodyearMoWater.rda   (new)
    goodyearMoWater0.rda  (old)
    3. Small changes to goodyearEDA2 mainly comments

--- RCode/goodyearEDA_2.R
+++ RCode/goodyearEDA_2.R
@@ -5,16 +5,16 @@ library( lubridate)
 library( viridis)
 library( scales)
 library( tidyverse); theme_set(theme_minimal())
-theme_update(panel.grid.minor = element_blank())
+    theme_update(panel.grid.minor = element_blank())
 suppressMessages(library( fields))
 
-head(goodyear)
+#head(goodyear)
 
 dfData <- as_tibble(goodyear)
 glimpse(dfData)
 
-dfDataC <- dfData[!is.na(dfData$Selenium), ]
-glimpse(dfDataC)
+dfDataSel <- dfData[!is.na(dfData$Selenium), ]
+glimpse(dfDataSel)
 
 #------------------------------------------------------
 
@@ -24,11 +24,11 @@ period2End <- ymd( "2015-04-01")
 period3End <- ymd( "2017-04-01" )
 periods <- c(period1End, period2End, period3End)
 
-# Only looking at bin1, bin5, bin7 and brine - same train
+# Only looking at brine and train 1 (bin1, bin5, bin7)
 drawTrain1FromPeriod <- function(startPeriod, endPeriod, interval_string){
-    resultPlot <- dfDataC %>% 
-        filter(dfDataC$ID == "Bin1" | dfDataC$ID == "Bin5" | dfDataC$ID == "Bin7" | 
-                   dfDataC$ID == "brine") %>% 
+    resultPlot <- dfDataSel %>% 
+        filter(dfDataSel$ID == "Bin1" | dfDataSel$ID == "Bin5" | dfDataSel$ID == "Bin7" | 
+                   dfDataSel$ID == "brine") %>% 
         filter(date >= startPeriod & date <= endPeriod ) %>% 
         ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
         geom_point(size = 2) +  
@@ -43,7 +43,7 @@ drawTrain1FromPeriod <- function(startPeriod, endPeriod, interval_string){
 
 #Selenium contents for train 1 for differnt periods
 #Period 1
-plotT1P1 <- drawTrain1FromPeriod(dfDataC$date[1], period1End, "4 months") + 
+plotT1P1 <- drawTrain1FromPeriod(dfDataSel$date[1], period1End, "4 months") + 
     labs(title= "Selenium content of Train 1 for period 1")
 plotT1P1
 
@@ -58,10 +58,13 @@ plotT1P3 <- drawTrain1FromPeriod(period2End, period3End, "4 months") +
 plotT1P3
 
 #Period 4
-plotT1P4 <- drawTrain1FromPeriod(period3End, tail(dfDataC$date, n = 1), "1 month") + 
+plotT1P4 <- drawTrain1FromPeriod(period3End, tail(dfDataSel$date, n = 1), "1 month") + 
     labs(title= "Selenium content of Train 1 for period 4")
 plotT1P4
 
+#TODO: fix period 2 x labels overlapping each other. Maybe disable legends 
+#   each plot and only show one on the right so that we have more drawing space.
+# Or maybe draw at a larger scale, current the plot set at 1280 x 735. 
 
 #draw grid function
 drawGrid2x2 <- function(plot1, plot2, plot3, plot4){
@@ -80,8 +83,8 @@ drawGrid2x2(plotT1P1, plotT1P2, plotT1P3, plotT1P4)
 
 #Selenium contents for train 4 for differnt periods
 drawTrain4FromPeriod <- function(startPeriod, endPeriod, interval_string){
-    resultPlot <- dfDataC %>% 
-        filter(dfDataC$ID == "Bin3" | dfDataC$ID == "brine") %>% 
+    resultPlot <- dfDataSel %>% 
+        filter(dfDataSel$ID == "Bin3" | dfDataSel$ID == "brine") %>% 
         filter(date >= startPeriod & date <= endPeriod ) %>% 
         ggplot(aes(x = date, y = Selenium, group = ID, color = ID)) + #aes_string because variable is not actual column factor term, but a name as string.+
         geom_point(size = 2) +  
@@ -96,7 +99,7 @@ drawTrain4FromPeriod <- function(startPeriod, endPeriod, interval_string){
 
 #Selenium contents for train 4 for differnt periods
 #Period 1
-plotT4P1 <- drawTrain4FromPeriod(dfDataC$date[1], period1End, "4 months") + 
+plotT4P1 <- drawTrain4FromPeriod(dfDataSel$date[1], period1End, "4 months") + 
     labs(title= "Selenium content of Train 4 for period 1")
 plotT4P1
 
@@ -111,7 +114,7 @@ plotT4P3 <- drawTrain4FromPeriod(period2End, period3End, "4 months") +
 plotT4P3
 
 #Period 4
-plotT4P4 <- drawTrain4FromPeriod(period3End, tail(dfDataC$date, n = 1), "1 month") + 
+plotT4P4 <- drawTrain4FromPeriod(period3End, tail(dfDataSel$date, n = 1), "1 month") + 
     labs(title= "Selenium content of Train 4 for period 4")
 plotT4P4
 
@@ -120,31 +123,33 @@ drawGrid2x2(plotT4P1, plotT4P2, plotT4P3, plotT4P4)
 #--------------------------------------
 
 #cleaning inflow and outflow
-dfDataFlow <- dfDataC[!is.na(dfDataC$Inflow), ]
-dfDataFlow <- dfDataFlow[!is.na(dfDataFlow$Outflow), ]
+dfDataSelFlow <- dfDataSel[!is.na(dfDataSel$Inflow), ]
+dfDataSelFlow <- dfDataSelFlow[!is.na(dfDataSelFlow$Outflow), ]
 
-dfDataFlow <- add_column(dfDataFlow, netflow = (dfDataFlow$Outflow - dfDataFlow$Inflow) ) 
+dfDataSelFlow <- add_column(dfDataSelFlow, 
+                            netflow = (dfDataSelFlow$Outflow - dfDataSelFlow$Inflow) ) 
 # glimpse(dfDataFlow)
 
 # regression analysis of Netflow vs Selenium for Train 1
-dfDataFlow %>% 
+dfDataSelFlow %>% 
     filter(ID == "Bin1" | ID == "Bin5" | ID == "Bin7") %>% 
     ggplot(aes(Selenium, netflow)) +
     geom_point(alpha = 1, aes(color = ID)) +
-    geom_smooth(formula = y~x, method = "lm") + # formula always look at the varaible in terms of x
+    geom_smooth(formula = y~x, method = "lm") + # formula always look at the variable in terms of x
     scale_color_viridis_d("Selenium and netflow") +
     xlab("selenium") +  
     ylab("netflow") +
     labs(title= "Selenium vs netflow RA for Train 1")
 
 # regression analysis of Netflow vs Selenium for Train 4
-dfDataFlow %>% 
+dfDataSelFlow %>% 
     filter(ID == "Bin3") %>% 
     ggplot(aes(Selenium, netflow)) +
     geom_point(alpha = 1, aes(color = ID)) +
-    geom_smooth(formula = y~x, method = "lm") + # formula always look at the varaible in terms of x
+    geom_smooth(formula = y~x, method = "lm") + 
     scale_color_viridis_d("Selenium and netflow") +
     xlab("selenium") + 
     ylab("netflow") +
     labs(title= "Selenium vs netflow RA for Train 4")
+
 
